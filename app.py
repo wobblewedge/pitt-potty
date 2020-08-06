9ac1b8addc66fc5b1e0a078b2befc51c43b69fc7
wobblewedge <andrew.d.brown1@gmail.com>
2020-08-05 23:19:52 -0400

    added abilities

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import tweepy
 import os
 from decouple import config
 import requests
+from PIL import Image
 
 CONSUMER_KEY=config('CONSUMER_KEY')
 CONSUMER_SECRET=config('CONSUMER_SECRET')
 ACCESS_TOKEN=config('ACCESS_TOKEN')
+
 ACCESS_TOKEN_SECRET=config('ACCESS_TOKEN_SECRET')
 BEARER_TOKEN=config('BEARER_TOKEN')
 ACCOUNT=config('ACCOUNT')
@@ -22,3 +24,5 @@ try:
     user = api.get_user(ACCOUNT)
 except tweepy.TweepError:
     print('Error! Failed to get request token.')
+
+api.update_status(status="And then...I got in.")
